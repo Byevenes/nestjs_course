c8efa1bb3e00195de92256f473d5c0332bd93c42
Byevenes <zneel.yevenes@gmail.com>
2023-06-08 23:36:40 -0400

    Dynamic Module

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', // TypeORM will automatically use the username and password from the environment variables
+      password: 'pass123',
+      database: 'postgres',
+    }),
+    CoffeesModule,
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule { }
