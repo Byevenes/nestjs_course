b641a1bdae7f65d36b2a34c1c129a62da5f0b29b
Byevenes <zneel.yevenes@gmail.com>
2023-07-20 14:17:43 -0400

    finished chapter 6, building blocks && solid principles

---
+++ src/common/interceptors/index.ts
@@ -0,0 +1,2 @@
+export * from './timeout/timeout.interceptor';
+export * from './wrap-response/wrap-response.interceptor';
--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -7,5 +7,9 @@ export const configValidationSchema = z
     DATABASE_USER: z.string(),
     DATABASE_PASSWORD: z.string(),
     DATABASE_NAME: z.string(),
+    STAGE: z.string().default('dev'),
+    API_KEY: z.string().min(10).max(20),
   })
   .required();
+
+export type configValidationType = z.infer<typeof configValidationSchema>;
