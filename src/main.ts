6fc22f3d376b84d081dd2c64224cb3ad7c7e731b
Byevenes <zneel.yevenes@gmail.com>
2023-07-24 20:13:54 -0400

    finished chapter 7 OpenApi Swagger

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import {
   TimeoutInterceptor,
   WrapResponseInterceptor,
 } from './common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // this will remove any properties that are not in the DTO
@@ -19,12 +21,26 @@ async function bootstrap() {
       },
     }),
   );
+
   app.useGlobalFilters(new HttpExceptionFilter()); // this will catch all the exceptions and return a custom response
   //app.useGlobalGuards(new ApiKeyGuard()); // this will apply the guards to all the routes
+
   app.useGlobalInterceptors(
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   ); // this will apply the interceptors to all the routes
+
+  // Setting up Swagger document
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
